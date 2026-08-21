8faa5a53da4e0c1d021fd63d878fef4bb7cacb31
fernando.tr96@outlook.es <fernando.tr96>
2026-08-20 21:30:59 -0600

    Fix: orderLine repository

--- ms-orders/src/main/java/com/ecomerce/ms_orders/repository/OrderLineItemsRepository.java
+++ ms-orders/src/main/java/com/ecomerce/ms_orders/repository/OrderLineItemsRepository.java
@@ -1,6 +1,7 @@
 package com.ecomerce.ms_orders.repository;
 
+import com.ecomerce.ms_orders.model.OrderLineItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderLineItemsRepository extends JpaRepository<OrderLineItemsRepository,Long> {
+public interface OrderLineItemsRepository extends JpaRepository<OrderLineItems,Long> {
 }
