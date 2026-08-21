638d93045a726f02c96d1a28d81f9452c2bc0a3b
fernando.tr96@outlook.es <fernando.tr96>
2026-08-20 20:34:14 -0600

    order entities

---
+++ ms-orders/src/main/java/com/ecomerce/ms_orders/model/Order.java
@@ -0,0 +1,26 @@
+package com.ecomerce.ms_orders.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true) // orphanRemoval es clave para Updates
+    @JoinColumn(name = "order_id") // Crea la FK en la tabla de items
+    private List<OrderLineItems> orderLineItemsList;
+}
